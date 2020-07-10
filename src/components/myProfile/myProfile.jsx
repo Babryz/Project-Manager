0ecc014aef4e5714770b56cdd38f65af62b05667
Emil Babra <brittsboemil@gmail.com>
2020-07-10 12:53:08 +0200

    Add button for editing profile

--- src/components/myProfile/myProfile.jsx
+++ src/components/myProfile/myProfile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './myProfile.css';
 
 import UserLogo from '../../pictures/user.png';
@@ -16,40 +17,41 @@ class MyProfile extends React.Component {
             <div className="container" id="my-profile">
                 <div className="user-info">
                     <img src={UserLogo} alt=""/>
-                    <h3>{user.firstName} "[Alias]" {user.lastName}</h3>
+                    <h3>{`${user.firstName} ${user.alias ? `"${user.alias}" ` : ''}${user.lastName}`}</h3>
                     <p>{user.email}</p>
                     <p>English, Swedish</p>
+                    <button className="standardBtn" id="editBtn"><Link to="/editProfile">Edit</Link></button>
                 </div>
                 <div className="other-info">
                     <div id="skills-info">
                         <h3>Skills</h3>
                         <ul>
-                            <li>Skill 1</li>
-                            <li>Skill 2</li>
-                            <li>Skill 3</li>
-                            <li>Skill 4</li>
-                            <li>Skill 5</li>
-                            <li>Skill 6</li>
-                            <li>Skill 7</li>
+                            <li>- Skill 1</li>
+                            <li>- Skill 2</li>
+                            <li>- Skill 3</li>
+                            <li>- Skill 4</li>
+                            <li>- Skill 5</li>
+                            <li>- Skill 6</li>
+                            <li>- Skill 7</li>
                         </ul>
                     </div>
                     <div id="projects-info">
                         <h3>Projects</h3>
                         <h4>Created:</h4>
                         <ul>
-                            <li>Project 1</li>
-                            <li>Project 2</li>
-                            <li>Project 3</li>
-                            <li>Project 4</li>
-                            <li>Project 5</li>
+                            <li>- Project 1</li>
+                            <li>- Project 2</li>
+                            <li>- Project 3</li>
+                            <li>- Project 4</li>
+                            <li>- Project 5</li>
                         </ul>
                         <h4>Joined:</h4>
                         <ul>
-                            <li>Project 1</li>
-                            <li>Project 2</li>
-                            <li>Project 3</li>
-                            <li>Project 4</li>
-                            <li>Project 5</li>
+                            <li>- Project 1</li>
+                            <li>- Project 2</li>
+                            <li>- Project 3</li>
+                            <li>- Project 4</li>
+                            <li>- Project 5</li>
                         </ul>
                     </div>
                     <div id="contacts-info">
